e220c96885e01d98e355c5b6fc52ee85fc190d3f
aaochoa <oe.anderson@cerezostudio.com>
2019-11-27 09:58:08 -0500

    Using infinite scroll - no filter

--- src/pokemon/PokemonCard.js
+++ src/pokemon/PokemonCard.js
@@ -42,10 +42,9 @@ export default class PokemonCard extends Component {
   }
 
   componentDidMount() {
-    const { name, url } = this.props;
-
-    const pokemonIndex = url.split("/")[url.split("/").length - 2];
-    const imageUrl = `https://github.com/PokeAPI/sprites/blob/master/sprites/pokemon/${pokemonIndex}.png?raw=true`;
+    const { name, url } = this.props
+    const pokemonIndex = url.split("/")[url.split("/").length - 2]
+    const imageUrl = `https://github.com/PokeAPI/sprites/blob/master/sprites/pokemon/${pokemonIndex}.png?raw=true`
 
     this.setState({ name, imageUrl, pokemonIndex });
   }
--- src/pokemon/PokemonList.js
+++ src/pokemon/PokemonList.js
@@ -2,23 +2,28 @@ import React, { Component } from 'react'
 import axios from 'axios'
 import PokemonCard from './PokemonCard'
 import app from '../base'
+import InfiniteScroll from "react-infinite-scroller"
 
 export default class PokemonList extends Component {
   constructor() {
     super()
     this.state = {
-      url: "https://pokeapi.co/api/v2/pokemon/",
+      url: "https://pokeapi.co/api/v2/pokemon/?limit=964",
       pokemon: null,
-      pokeElements : []
+      pokeElements : [],
+      items: 43,
+      dataLoaded: false,
+      hasMoreItems: true
     }
   }
-
+  
   async componentDidMount() {
     const res = await axios.get(this.state.url)
     let pokeArray = this.cards(res.data['results'])
     this.setState({
       pokemon: res.data['results'], 
-      pokeElements: pokeArray
+      pokeElements: pokeArray,
+      dataLoaded: true
     })
   } 
 
@@ -35,11 +40,25 @@ export default class PokemonList extends Component {
   }
 
   cards = (pokemon) => {
-    let pokeArray = []
-    pokemon.forEach(pokemon => {
-      pokeArray.push(<PokemonCard key={pokemon.name} name={pokemon.name} url={pokemon.url} />)
-    })
-    return pokeArray
+    let pokeCardsArray = []
+    if (pokemon.length > 0) {
+      for (let i = 0; i < this.state.items; i++) {
+        if (i < 964) {
+          pokeCardsArray.push(<PokemonCard key={pokemon[i].name} name={pokemon[i].name} url={pokemon[i].url} />)
+        }
+      }
+      return pokeCardsArray
+    } else return <h1>Loading...</h1>
+  }
+
+  loadMore() {
+    if (this.state.items < 963) {
+      this.setState({ hasMoreItems: false})
+    } else {
+      setTimeout(() => {
+        this.setState({ items: this.state.items + 40})
+      }, 2000)
+    }
   }
  
   render () {
@@ -53,13 +72,13 @@ export default class PokemonList extends Component {
             </div>
           </div>
         </div>
-        {this.state.pokemon ? (
-          <div className="row">
-            {this.state.pokeElements}
-          </div>
-        ) : (
-          <h1>Cargando...</h1>
-        )}
+        <InfiniteScroll className="row" key="scroller" id="scroller" loadMore={this.loadMore.bind(this)} hasMore={this.state.hasMoreItems} loader={<div className="loader"> Loading... </div>} useWindow={false}>
+          {this.state.pokemon ? (
+            this.cards(this.state.pokemon)
+          ) : (
+            <h1>Loading...</h1>
+          )}
+        </InfiniteScroll>
       </div>
     )
   }
